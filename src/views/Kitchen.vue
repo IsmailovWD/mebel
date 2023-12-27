556f16fbefbb10cdec38766ef96d37b2c936a639
ismailovWD <ismailovWD@gmail.com>
2023-12-27 20:39:27 +0500

    commit

--- src/views/Kitchen.vue
+++ src/views/Kitchen.vue
@@ -30,47 +30,56 @@
                 </n-button>
             </div>
             <img style="width: 100%;" src="../assets/image/Kitchen/default.jpg" alt="">
-            <img v-if="topactive" style="position: absolute;top: 0; left: 0; width: 100%;" :src="topactive" alt="">
-            <img v-if="leftactive" style="position: absolute;top: 0; left: 0; width: 100%;" :src="leftactive" alt="">
-            <img v-if="bottomactive" style="position: absolute;top: 0; left: 0; width: 100%;" :src="bottomactive" alt="">
-            <img v-if="rightactive" style="position: absolute;top: 0; left: 0; width: 100%;" :src="rightactive" alt="">
+            <img v-if="topactive == '1'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/top/result/1.png" alt="">
+            <img v-if="topactive == '2'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/top/result/2.png" alt="">
+            <img v-if="topactive == '3'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/top/result/3.png" alt="">
+            <img v-if="topactive == '4'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/top/result/4.png" alt="">
+
+            <img v-if="leftactive == '1'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/left/result/1.png" alt="">
+            <img v-if="leftactive == '2'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/left/result/2.png" alt="">
+
+            <img v-if="bottomactive == '1'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/bottom/result/1.png" alt="">
+            <img v-if="bottomactive == '2'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/bottom/result/2.png" alt="">
+
+            <img v-if="rightactive == '1'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/right/result/1.png" alt="">
+            <img v-if="rightactive == '2'" style="position: absolute;top: 0; left: 0; width: 100%;" src="../assets/image/Kitchen/right/result/2.png" alt="">
         </div>
         <div style="height: 100vh; position: sticky; top: 0;">
             <div v-if="tops" id="top" style="display: flex; flex-direction: column;">
-                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="topactive = '/src/assets/image/Kitchen/top/result/1.png'">
+                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="topactive = '1'">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/1.jpg" alt="">
                 </div>
-                <div @click="topactive = '/src/assets/image/Kitchen/top/result/2.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="topactive = '2'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/2.jpg" alt="">
                 </div>
-                <div @click="topactive = '/src/assets/image/Kitchen/top/result/3.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="topactive = '3'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/3.jpg" alt="">
                 </div>
-                <div @click="topactive = '/src/assets/image/Kitchen/top/result/4.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="topactive = '4'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/4.jpg" alt="">
                 </div>
             </div>
             <div v-if="lefts" id="top" style="display: flex; flex-direction: column;">
-                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="leftactive = '/src/assets/image/Kitchen/left/result/1.png'">
+                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="leftactive = '1'">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/left/1.jpg" alt="">
                 </div>
-                <div @click="leftactive = '/src/assets/image/Kitchen/left/result/2.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="leftactive = '2'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/left/2.jpg" alt="">
                 </div>
             </div>
             <div v-if="bottoms" id="top" style="display: flex; flex-direction: column;">
-                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="bottomactive = '/src/assets/image/Kitchen/bottom/result/1.png'">
+                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="bottomactive = '1'">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/1.jpg" alt="">
                 </div>
-                <div @click="bottomactive = '/src/assets/image/Kitchen/bottom/result/2.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="bottomactive = '2'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/top/4.jpg" alt="">
                 </div>
             </div>
             <div v-if="rights" id="top" style="display: flex; flex-direction: column;">
-                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="rightactive = '/src/assets/image/Kitchen/right/result/1.png'">
+                <div style="height: 90px; overflow: hidden; cursor: pointer;" @click="rightactive = '1'">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/right/1.jpg" alt="">
                 </div>
-                <div @click="rightactive = '/src/assets/image/Kitchen/bottom/right/2.png'" style="height: 90px; overflow: hidden; cursor: pointer;">
+                <div @click="rightactive = '2'" style="height: 90px; overflow: hidden; cursor: pointer;">
                     <img style="width: 100%; height: 100px;" src="../assets/image/Kitchen/right/2.jpg" alt="">
                 </div>
             </div>
